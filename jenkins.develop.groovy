e1c40feb39cdacaa4ed3dd9e34ec166e867357f2
KHEfreiIntervenant <71932335+KHEfreiIntervenant@users.noreply.github.com>
2020-11-24 07:25:46 -0800

    Create jenkins.develop.groovy

---
+++ jenkins.develop.groovy
@@ -0,0 +1,17 @@
+def build_app(){
+  sh 'docker-compose up -d'
+}
+
+def test_app(){
+  sh 'python test_app.py'
+}
+
+def down_app(){
+  sh 'docker-compose down'
+}
+
+def release_app(){
+  echo 'branch into release'
+}
+
+return this
